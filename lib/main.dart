ed20ed6b729d120c28530b2e54b6515a5fd84f03
Jaden Scheidegger <jadenvidz@gmail.com>
2020-07-01 09:40:47 +0200

    image wird angezeigt

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_editor_pro/image_editor_pro.dart';
-
+import 'package:image_picker/image_picker.dart';
 
 
 void main(){
@@ -17,6 +17,60 @@ class LandingScreen extends StatefulWidget {
 }
 
 class _LandingScreenState extends State<LandingScreen> {
+
+  File imageFile;
+  final picker = ImagePicker();
+
+  _openGallary(BuildContext context) async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    this.setState(() {
+      imageFile = File(pickedFile.path);
+    });
+    Navigator.of(context).pop();
+  }
+  _openCamera(BuildContext context) async{
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+    this.setState(() {
+      imageFile = File(pickedFile.path);
+    });
+    Navigator.of(context).pop();
+  }
+  //no mistakes grr
+
+  Future<void> _showChoiceDialog(BuildContext context) {
+    return showDialog(context: context,builder: (BuildContext context){
+      return AlertDialog(
+        title: Text("bruh?"),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              GestureDetector(
+                child: Text("Gallery"),
+                onTap: (){
+                  _openGallary(context);
+                },
+              ),
+              Padding(padding: EdgeInsets.all(6.0)),
+              GestureDetector(
+                child: Text("Camera"),
+                onTap: (){
+                  _openCamera(context);
+                },
+              )
+            ],
+          ),
+        ),
+      );
+    });
+  }
+
+  Widget _isImageView(){
+    if (imageFile == null) {
+      return Text("No Image Selected");
+    } else {
+      Image.file(imageFile,width: 400,height: 400);
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,9 +82,9 @@ class _LandingScreenState extends State<LandingScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
-              Text("No Image Selected"),
+              _isImageView(),
               RaisedButton(onPressed: (){
-
+                _showChoiceDialog(context);
               },child: Text("Select your mom"),)
             ],
           ),
